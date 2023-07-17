df3bf6462982c2836af4ca416972f8b72771b7fa
vignesh nr <vigneshnr@sulekha.com>
2023-07-17 15:44:53 +0530

    test server application on elastic bean stalk

---
+++ express.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route handler for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+
+app.get('/app',(req,res)=>{
+    res.send("it's just a testing application for server side rendering")
+})
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port http://localhost:${port}`);
+});
